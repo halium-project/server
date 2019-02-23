4b0d8de1f2c706962063b42d69c554e57408c536
Peltoche <dev@halium.fr>
2019-02-23 16:14:55 +0100

    Add a contact.ControllerMock

---
+++ resource/contact/controller_mock.go
@@ -0,0 +1,39 @@
+package contact
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type ControllerMock struct {
+	mock.Mock
+}
+
+func (t *ControllerMock) Create(ctx context.Context, cmd *CreateCmd) error {
+	return t.Called(cmd).Error(0)
+}
+
+func (t *ControllerMock) Get(ctx context.Context, cmd *GetCmd) (*Contact, error) {
+	args := t.Called(cmd)
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*Contact), args.Error(1)
+}
+
+func (t *ControllerMock) GetAll(ctx context.Context, cmd *GetAllCmd) (map[string]Contact, error) {
+	args := t.Called(cmd)
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(map[string]Contact), args.Error(1)
+}
+
+func (t *ControllerMock) Delete(ctx context.Context, cmd *DeleteCmd) error {
+	return t.Called(cmd).Error(0)
+}
---
+++ resource/contact/controller_mock_test.go
@@ -0,0 +1,101 @@
+package contact
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Contact_ControllerMock_Create(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("Create", &CreateCmd{
+		Name: ValidContact.Name,
+	}).Return(fmt.Errorf("some-error")).Once()
+
+	err := mock.Create(context.Background(), &CreateCmd{
+		Name: ValidContact.Name,
+	})
+
+	assert.EqualError(t, err, "some-error")
+
+	mock.AssertExpectations(t)
+}
+
+func Test_Contact_ControllerMock_Get(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("Get", &GetCmd{
+		ContactID: "some-contact-id",
+	}).Return(&ValidContact, nil).Once()
+
+	contact, err := mock.Get(context.Background(), &GetCmd{
+		ContactID: "some-contact-id",
+	})
+
+	assert.NoError(t, err)
+	assert.EqualValues(t, &ValidContact, contact)
+
+	mock.AssertExpectations(t)
+}
+
+func Test_Contact_ControllerMock_Get_with_error(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("Get", &GetCmd{
+		ContactID: "some-contact-id",
+	}).Return(nil, fmt.Errorf("some-error")).Once()
+
+	contact, err := mock.Get(context.Background(), &GetCmd{
+		ContactID: "some-contact-id",
+	})
+
+	assert.Nil(t, contact)
+	assert.EqualError(t, err, "some-error")
+
+	mock.AssertExpectations(t)
+}
+
+func Test_Contact_ControllerMock_GetAll(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("GetAll", &GetAllCmd{}).Return(map[string]Contact{
+		"some-id": ValidContact,
+	}, nil).Once()
+
+	res, err := mock.GetAll(context.Background(), &GetAllCmd{})
+
+	assert.NoError(t, err)
+	assert.EqualValues(t, map[string]Contact{
+		"some-id": ValidContact,
+	}, res)
+
+	mock.AssertExpectations(t)
+}
+
+func Test_Contact_ControllerMock_GetAll_with_error(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("GetAll", &GetAllCmd{}).Return(nil, fmt.Errorf("some-error")).Once()
+
+	contact, err := mock.GetAll(context.Background(), &GetAllCmd{})
+
+	assert.Nil(t, contact)
+	assert.EqualError(t, err, "some-error")
+
+	mock.AssertExpectations(t)
+}
+
+func Test_Contact_ControllerMock_Delete(t *testing.T) {
+	mock := new(ControllerMock)
+
+	mock.On("Delete", &DeleteCmd{ContactID: "some-id"}).Return(nil).Once()
+
+	err := mock.Delete(context.Background(), &DeleteCmd{ContactID: "some-id"})
+
+	assert.NoError(t, err)
+
+	mock.AssertExpectations(t)
+}
